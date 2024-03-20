ac2dd7ed17fa9c816b507029acaa20957e876777
opielapatryk <xpatrykopiela@gmail.com>
2024-03-20 12:54:10 +0100

    Implemented subtraction feature

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
 class SimpleCalculator:
-    def add(self,a,b,c=0):
-        return a + b + c
+    def add(self,*args):
+        return sum(args)
+    
+    def subtract(self,a,b):
+        return a - b
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,12 +3,13 @@
 
 from simple_calculator.main import SimpleCalculator
 
-def test_add_two_numbers():
+def test_add_many_numbers():
+    numbers = range(100)
     calculator = SimpleCalculator()
-    result = calculator.add(4,5)
-    assert result == 9
+    result = calculator.add(*numbers)
+    assert result == 4950
 
-def test_add_three_numbers():
+def test_subtract_two_numbers():
     calculator = SimpleCalculator()
-    result = calculator.add(4,5,6)
-    assert result == 15
+    result = calculator.subtract(6,4)
+    assert result == 2
